a1dd350e9e39d719e61efe6ce759f2538dd2bdd8
unknown <ab2888@scotchmel.vic.edu.au>
2012-10-01 10:08:58 +1000

    index.php

---
+++ url_converter.js
@@ -0,0 +1,17 @@
+function f () {
+var fullReq;
+var toklong;
+var timelong;
+var tok;
+var time;
+var a;
+var b;
+fullReq=location.search;
+a=fullReq.substr(1);
+b=a.split("&");
+toklong=b[0].split("=");
+tok=toklong[1];
+timelong=b[1].split("=");
+time=timelong[1];
+document.getElementById("a_tok").value=tok;
+document.getElementById("expire").value=time;}
