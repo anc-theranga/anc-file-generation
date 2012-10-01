4ba0344291178baacce166b6ada3f7cb4a0c547f
unknown <ab2888@scotchmel.vic.edu.au>
2012-10-01 10:22:58 +1000

    index.php

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 <html lang="en">
   <body>
    <h1>Authentication successful</h1>
-<form action="https://localhost:84201/" method="get">
-<input type="hidden" id="a_tok" name="a_tok" value="" autocomplete="off" />
-<input type="hidden" id="expire" name="expire" value="" autocomplete="off" />
-<input type="submit" value="Click to continue!" />
-</form>
+    <a href="" id="url">Click to continue!</a>
 <script type="text/javascript" src="url_converter.js"></script>
 <script type="text/javascript">
-f();
+f("url");
 </script>
   </body>
 </html>
--- url_converter.js
+++ url_converter.js
@@ -1,15 +1,19 @@
-function f () {
+function f (var in) {
 var fullReq;
 var toklong;
 var timelong;
 var tok;
 var time;
 var b;
+var ret;
+var c;
+var d;
 fullReq=location.search;
 b=fullReq.split("&");
 toklong=b[0].split("=");
 tok=toklong[1];
 timelong=b[1].split("=");
 time=timelong[1];
-document.getElementById("a_tok").value=tok;
-document.getElementById("expire").value=time;}
+var a="https://localhst:84201/?tok=";
+ret=a.concat(tok,"&time=",time);
+document.getElementById(in).href=ret;}
