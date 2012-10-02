857f97881d74fa0ea5beef862b220878e6546870
unknown <ab2888@scotchmel.vic.edu.au>
2012-10-02 16:32:08 +1000

    index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <script>
-   function change () {
-    var goto="https://127.0.0.1:84201/";
-    var nHref=goto.concat(window.location);
-    window.location.replace(nHref); }
-  </script>
+  <script src="url_converter.js" />
  </head>
  <body onload="change()">
   <h1>Redirecting...</h1>
