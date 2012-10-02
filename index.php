ca563e43362be6dcea8aa7af96243a10ab2f69a1
unknown <ab2888@scotchmel.vic.edu.au>
2012-10-02 17:00:33 +1000

    index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
+<?php
+$loc="https://127.0.0.1/";
+$nLoc=$loc . $_SERVER["REQUEST_URI"];
+header($nLoc,true,302);
+?>
+
 <!DOCTYPE html>
 <html>
  <head>
-  <script src="url_converter.js"></script>
  </head>
- <body onload="change()">
-  <h1>Redirecting...</h1>
-  <p>If you are not automatically redirected,</p>
-  <button type="button" onclick="change()">click here</button>
+ <body>
+  <h1>Redirecting...<br><br></h1>
+  <h3>As you have not been automatically redirected, type <code>https://127.0.0.1:84201/</code> before the URL current in your browser's address bar</h3>
  </body>
 </html>
