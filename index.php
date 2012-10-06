ae02fa5e04148683535de10fe241f9e0140b8631
unknown <ab2888@scotchmel.vic.edu.au>
2012-10-06 18:32:50 +1000

    index.php

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <?php
-$loc="https://127.0.0.1/";
-$nLoc=$loc . $_SERVER["REQUEST_URI"];
-header($nLoc,true,302);
 ?>
 
 <!DOCTYPE html>
 <html>
- <head>
- </head>
- <body>
-  <h1>Redirecting...<br><br></h1>
-  <h3>As you have not been automatically redirected, type <code>https://127.0.0.1:84201/</code> before the URL current in your browser's address bar</h3>
- </body>
+<head>
+<script>
+function func ()
+{
+var poshash;
+var posip;
+var posamp;
+var ip;
+var args;
+var firstPart;
+var url=new String (location);
+var nURI;
+poshash=url.indexOf("#access_token");
+posip=url.indexOf("?ip=");
+posip=posip+4;
+ip=url.substring(posip,poshash);
+posamp=url.indexOf("&");
+args=url.substring(poshash+1);
+firstPart="http://";
+nURI=firstPart.concat(ip,"/?",args);
+window.location.replace(nURI);
+}
+</script>
+</head>
+<body onload="func()">
+<h1>Redirecting...<br><br></h1>
+<h3>If you are not redirected in 5 seconds,</h3>
+<button type="button" onlick="func()">click here</button>
+</body>
 </html>
